c5e78de32dd479e99e3d38dd4159cfbae39b0943
Nicholas Arnaud <cobrahead09@gmail.com>
2018-04-10 16:27:36 -0500

    Added project

---
+++ Interfaces/Interface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Interfaces
+{
+    public interface IEnumerable<T>
+    {
+
+        T Add<T>(T a, T b);
+    }
+
+    public class Interface<T> : IEnumerable<T>
+    {
+        public T Add<T>(T a, T b)
+        {
+            var c = (int)(object)b;
+            var d = (int)(object)a;
+            var total = d + c;
+            return (T)(object)total;
+        }
+    }
+}
